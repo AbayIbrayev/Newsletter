13d112c2ce56662569076cd6dcf8877e1528989a
Abay <abayibrayev7@gmail.com>
2020-02-15 11:04:01 +0000

    data is not parsed using a POST request from the inputs using body-parser

--- app.js
+++ app.js
@@ -4,8 +4,24 @@ const express = require("express"),
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  let firstName = req.body.fName,
+    lastName = req.body.lName,
+    email = req.body.email;
+
+  console.log(firstName, lastName, email);
 });
